66e09a43eceab9fc68b3eff0933631f08202168a
Juan Manuel Berros <juanmaberros@gmail.com>
2018-02-07 14:41:05 -0300

    updated version number and changelog

--- bed_to_tabix/package_info.py
+++ bed_to_tabix/package_info.py
@@ -1,7 +1,7 @@
 PACKAGE_INFO = dict(
     PROGRAM_NAME = 'bed_to_tabix',
     DESCRIPTION = 'Download the 1kG genotypes of the regions in a .bed file',
-    VERSION = '1.1.1',
+    VERSION = '1.1.2',
     DATE = '20 Nov 2016',
     AUTHOR = 'Juan Manuel Berros',
     AUTHOR_EMAIL = 'juanma.berros@gmail.com',
