547de040bb87bc95b55953990d6aad6966216f19
Juan Manuel Berros <juanmaberros@gmail.com>
2020-01-03 18:44:59 -0300

    low_memory=False to prevent warning on reading BED

--- bed_to_tabix/lib/read_bed.py
+++ bed_to_tabix/lib/read_bed.py
@@ -6,6 +6,7 @@ from ..lib import make_chromosome_series_categorical
 
 def read_bed(bedfile):
     """Read a bedfile and return a pd.DataFrame with the regions."""
-    df = pd.read_csv(bedfile, sep=r"\s+", names=BED_COLUMNS)
+    df = pd.read_csv(bedfile, sep=r"\s+", names=BED_COLUMNS,
+                     low_memory=False)
     df['chrom'] = make_chromosome_series_categorical(df['chrom'])
     return df
